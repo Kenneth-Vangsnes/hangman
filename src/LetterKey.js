3cc4dddcdeb2a133868a2df47a512ca7f619338b
Arfaleas <vangsnesk@gmail.com>
2023-08-29 13:47:18 +0200

    formatted with prettier

--- src/LetterKey.js
+++ src/LetterKey.js
@@ -1,18 +1,20 @@
-import React from "react";
+import React from "react"
 
 export default function LetterKey(props) {
-    const styles = {backgroundColor: props.isClicked ? "orange" : "green"}
+  const styles = {
+    backgroundColor: props.isClicked ? "orange" : "green",
+    cursor: props.isClicked ? "undefined" : "pointer",
+  }
 
-    return (
-        <div 
-            className={props.className}
-            style={styles}
-            key={props.value}
-            value={props.value}
-            clicked={props.isClicked}
-            onClick={() => props.onClick(props.children)}
-            >
-            {props.children}
-        </div>
-    )
+  return (
+    <div
+      className={props.className}
+      style={styles}
+      key={props.value}
+      value={props.value}
+      clicked={props.isClicked}
+      onClick={() => props.onClick(props.children)}>
+      {props.children}
+    </div>
+  )
 }
